8bfb715b3ccd5e2b0b8b2f173813be08d2a8ffdc
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2025-03-25 18:39:07 +0100

    wip show 3d model in monument details

--- app/components/Model.tsx
+++ app/components/Model.tsx
@@ -1,30 +1,66 @@
-// import React, { Suspense } from 'react';
-// import { Canvas } from '@react-three/fiber';
-// import { useGLTF, OrbitControls } from '@react-three/drei';
-// import { View } from 'react-native';
-//
-// const localhostIP = '192.168.2.134';
-// const port = '3000';
-//
-// const MODEL_URL = `http://${localhostIP}:${port}/Hand.glb`;
-//
-// function HandModel() {
-//     const { scene } = useGLTF(MODEL_URL);
-//     console.log("...loading 3D model");
-//     return <primitive object={scene} scale={0.5} />;
-// }
-//
-// export default function ShowModel() {
+import React, {useRef} from "react";
+import {BACKEND_BASE_URL} from "@/src/constants";
+import {useGLTF} from "@react-three/drei";
+import {Mesh, MeshStandardMaterial, Object3D, TextureLoader} from "three";
+import {useLoader} from "@react-three/fiber";
+
+type MonumentModelProps = {
+    modelUrl: string;
+};
+
+export default function Model({ modelUrl }: MonumentModelProps) {
+    // const group = useRef();
+    // const fullUrl = `${BACKEND_BASE_URL}${modelUrl}`;
+    const fullUrl = `${BACKEND_BASE_URL}/Duck.glb`;
+    console.log(fullUrl);
+    // const gltf = useLoader(GLTFLoader, fullUrl);
+    const gltf = useGLTF(fullUrl);
+    console.log("gltf: ", gltf);
+    // console.log("gltf scene:", gltf.scene);
+    // console.log("materials:", gltf.materials);
+    gltf.scene.traverse((child: Object3D) => {
+        if (child instanceof Mesh) {
+            if (child.material instanceof MeshStandardMaterial) {
+                // if (child.material.map) {
+                //     child.material.map.name = "";
+                //     child.material.map.userData = {
+                //         mimeType: "image/png",
+                //     }
+                // }
+                console.log("texture:", child.material.map);
+                const texture = new TextureLoader().load(`${BACKEND_BASE_URL}/Image_0.png`);
+                // texture.colorSpace = "srgb";
+                // texture.flipY = false;
+                // texture.userData = {
+                //     mimeType: "image/png",
+                // };
+                child.material.map = texture;
+                console.log("texture:", child.material.map);
+                // child.material.color.set(0xFF0000);
+                child.material.flatShading = false;
+                child.material.needsUpdate = true;
+            }
+        }
+    });
+    return <primitive object={gltf.scene} scale={2}/>
+}
+
+// export default function Model() {
+//     const { nodes, materials } = useGLTF(`${BACKEND_BASE_URL}/OmaniCupBlender.glb`);
+//     console.log("nodes:", nodes);
+//     console.log(materials.main);
 //     return (
-//         <View style={{ flex: 1 }}>
-//             <Canvas>
-//                 <ambientLight intensity={0.5} />
-//                 <directionalLight position={[5, 5, 5]} intensity={1} />
-//                 <Suspense fallback={null}>
-//                     <HandModel />
-//                 </Suspense>
-//                 <OrbitControls /> {/* Allows rotating the model */}
-//             </Canvas>
-//         </View>
-//     );
+//         <group dispose={null}>
+//             {/*<mesh castShadow receiveShadow geometry={nodes.mesh.geometries} material={materials.main} />*/}
+//             {/*<mesh castShadow receiveShadow material={materials.main} />*/}
+//             <mesh
+//                 castShadow
+//                 receiveShadow
+//                 geometry={nodes.mesh.geometries}
+//                 material={materials['main.003']}
+//             />
+//         </group>
+//     )
 // }
+
+useGLTF.preload(`${BACKEND_BASE_URL}/OmaniCupBlender.glb`);
--- app/components/ShowModel.tsx
+++ app/components/ShowModel.tsx
@@ -1,8 +1,13 @@
-import React, {Suspense} from "react";
-import {useLoader} from "@react-three/fiber";
-import {ActivityIndicator} from "react-native";
+import React, {Suspense, useRef} from "react";
+import {Canvas, useLoader} from "@react-three/fiber";
+import {ActivityIndicator, View} from "react-native";
 import {BACKEND_BASE_URL} from "@/src/constants";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
+import {Environment, PerspectiveCamera, useGLTF} from "@react-three/drei";
+import Model from "@/app/components/Model";
+import {GLView} from "expo-gl";
+import {Scene} from "three";
+import Renderer from "three/src/renderers/common/Renderer";
 
 type MonumentModelProps = {
     modelUrl: string;
@@ -10,31 +15,62 @@ type MonumentModelProps = {
 
 const MonumentModel: React.FC<MonumentModelProps> = ({ modelUrl }) => {
 
-    function Model({ modelUrl }: MonumentModelProps) {
-        const fullUrl = `${BACKEND_BASE_URL}${modelUrl}`;
-        console.log(fullUrl);
-        const gltf = useLoader(GLTFLoader, fullUrl);
-        return <primitive object={gltf.scene} />
+    // function Model({ modelUrl }: MonumentModelProps) {
+    //     const groupRef = useRef();
+    //     const fullUrl = `${BACKEND_BASE_URL}${modelUrl}`;
+    //     console.log(fullUrl);
+    //     // const gltf = useLoader(GLTFLoader, fullUrl);
+    //     // return <primitive object={gltf.scene} />
+    //     const { nodes, materials } = useGLTF(fullUrl);
+    //     return (
+    //         <group ref={groupRef} dispose={null}>
+    //             <mesh material={materials.main} />
+    //         </group>
+    //     );
+    // }
+
+    const onContextCreate = async () => {
+        return (
+        <Canvas camera={{ position: [-6, 0, 16], fov: 36 }}
+                onCreated={(state) =>
+                { const _gl = state.gl.getContext()
+                    const pixelStorei = _gl.pixelStorei.bind(_gl)
+                    _gl.pixelStorei = function(...args) {
+                        const [parameter] = args
+                        switch(parameter) { case _gl.UNPACK_FLIP_Y_WEBGL: return pixelStorei(...args) } } }}>
+            <ambientLight intensity={2} />
+            <directionalLight position={[5, 5, 5]} intensity={2} />
+            {/*<Environment preset="city" />*/}
+            <Suspense fallback={"...loading"}>
+                <Model modelUrl={modelUrl}></Model>
+                {/*<Model></Model>*/}
+            </Suspense>
+            {/*<OrbitControls />*/}
+        </Canvas>
+        );
     }
 
     return (
-        <Suspense fallback={<ActivityIndicator size="large" color="#0000ff" />}>
-            <Model modelUrl={modelUrl}></Model>
-        </Suspense>
+        <View style={{ width: "100%", height: 300 }}>
+            {/*<GLView onContextCreate={onContextCreate}></GLView>*/}
+            <Canvas camera={{ position: [-6, 0, 16], fov: 36 }}
+                    onCreated={(state) =>
+                    { const _gl = state.gl.getContext()
+                        const pixelStorei = _gl.pixelStorei.bind(_gl)
+                        _gl.pixelStorei = function(...args) {
+                            const [parameter] = args
+                            switch(parameter) { case _gl.UNPACK_FLIP_Y_WEBGL: return pixelStorei(...args) } } }}>
+                <ambientLight intensity={2} />
+                <directionalLight position={[5, 5, 5]} intensity={2} />
+                {/*<Environment preset="city" />*/}
+                <Suspense fallback={"...loading"}>
+                    <Model modelUrl={modelUrl}></Model>
+                    {/*<Model></Model>*/}
+                </Suspense>
+                {/*<OrbitControls />*/}
+            </Canvas>
+        </View>
     );
-
-    // return (
-    //     <View style={{ width: "100%", height: 300 }}>
-    //         <Canvas camera={{ position: [0, 1, 3] }}>
-    //             <ambientLight intensity={1} />
-    //             <directionalLight position={[2, 5, 2]} intensity={1.5} />
-    //             <Suspense fallback={<ActivityIndicator size="large" color="#0000ff" />}>
-    //                 <Model modelUrl={modelUrl}></Model>
-    //             </Suspense>
-    //             <OrbitControls />
-    //         </Canvas>
-    //     </View>
-    // );
 };
 
 export default MonumentModel;
