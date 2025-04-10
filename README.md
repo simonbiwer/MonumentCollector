96c3df46e2cc7f097b8029bf692933b7818f56c6
simonbiwer <91076673+simonbiwer@users.noreply.github.com>
2025-04-10 15:53:58 +0200

    adjust readme and comments

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Monument Collector
+# Arqiv
 
-### Start application
+## Description
+An App that allows you to create a personal collection
+of 3D-Models of museum exhibits.
+For collection the app uses NFC technology.
+The server application where the 3D-Models and collections
+of users are stored is https://github.com/simonbiwer/arqiv-backend
+
+## Start application
 
 - Install dependencies `npm install`
-- Start server application for 3D models `arqiv` (https://github.com/simonbiwer/arqiv)
+- Start server application `arqiv-backend` (https://github.com/simonbiwer/arqiv-backend)
 - If needed adjust `localhostIP` and `port` in [constants](src/constants.ts)
 - Start the application with `expo start` (to show the app in the browser use `--web`)
-- Note: If you want to use --web the nfc utils need to be disabled, because they are only available on phones
--- src/nfcUtils.ts
+++ src/nfcUtils.ts
@@ -1,18 +1,14 @@
 import { Alert, Platform } from "react-native";
 import NfcManager, { Ndef, NfcTech } from "react-native-nfc-manager";
-import { Monument } from "@/src/Monument";
-import { BACKEND_BASE_URL, COLLECTION_API_URL } from "@/src/constants";
-import { getUserId } from "@/src/userIdUtils";
-import { updateCollection } from "@/src/arqivApiClient";
 
-// Nur auf iOS/Android starten – NICHT im Web
+// Only start for iOS/Android – NOT for web
 if (Platform.OS !== 'web' && NfcManager?.start) {
     NfcManager.start();
     NfcManager.cancelTechnologyRequest();
 }
 
 export async function readNFC(): Promise<string | undefined> {
-    // NFC auf Web-Plattform deaktivieren
+    // Disable NFC for Web
     if (Platform.OS === 'web') {
         Alert.alert("NFC not supported on Web");
         return;
@@ -25,7 +21,7 @@ export async function readNFC(): Promise<string | undefined> {
         const payload = tag?.ndefMessage?.[0]?.payload;
         Alert.alert('NFC Tag Detected!', JSON.stringify(tag));
 
-        // @ts-ignore: decodePayload is safe hier
+        // @ts-ignore: decodePayload is safe here
         return Ndef.text.decodePayload(payload);
     } catch (error) {
         console.warn('NFC Error:', error);
